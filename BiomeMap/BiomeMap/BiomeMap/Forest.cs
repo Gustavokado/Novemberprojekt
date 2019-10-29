f6d98570c4c6ad4d431e27767ac2d481bcddb430
Gustavokado <gusmel79@gmail.com>
2019-10-29 13:12:28 +0100

    Mountain first draft

--- BiomeMap/BiomeMap/BiomeMap/Forest.cs
+++ BiomeMap/BiomeMap/BiomeMap/Forest.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BiomeMap
 {
-    class Mountain: Tile
+    class Forest:Tile
     {
-        public Mountain(int x, int y, int startX, int startY, int size) : base(x, y, startX, startY, size)
+        
+        public Forest(int x, int y, int startX, int startY, int size) : base(x, y, startX, startY, size)
         {
-            biome = "mountain";
-            display = "A";
-            CheckNearby(startX, startY, size);
+            biome = "forest";
+            display = "T";
+            CheckNearby(startX, startY, size);           
         }
+
         public override void printTile(int topPosX, int topPosY)
         {
-            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.CursorLeft = (x * 2) + topPosX * 2;
             Console.CursorTop = y + topPosY;
             Console.Write("██");
@@ -29,9 +32,9 @@ namespace BiomeMap
 
             Tile newTile = tiles[x, y];
 
-            if (newTile.biome != "mountain" && !newTile.noSpread)
+            if (newTile.biome != "forest" && !newTile.noSpread)
             {
-                
+                random.Next(0, size / 2);
 
                 //Thread.Sleep(5);
 
@@ -41,14 +44,14 @@ namespace BiomeMap
                 amount++;
                 int xDif = startX - x;
                 int yDif = startY - y;
-                if (Tile.random.Next(size)/ (Math.Sqrt(xDif * xDif + yDif * yDif)) > Math.Sqrt(xDif * xDif + yDif * yDif))
+                if (Tile.random.Next(size) > Math.Sqrt(xDif * xDif + yDif * yDif))
                 {
 
-                    newTile = new Mountain(x, y, startX, startY, size);
+                    newTile = new Forest(x, y, startX, startY, size);
                     //this.printTile(0, 0);
                 }
                 newTile.noSpread = true;
-            }
+            }                      
         }
     }
 }
---
+++ BiomeMap/BiomeMap/BiomeMap/River.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiomeMap
+{
+    class River
+    {
+        public River(int x, int y) 
+        {
+
+        }
+    }
+}
